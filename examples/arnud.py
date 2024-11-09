bc089842c4190097470a861a09fd6ac9848658b5
markgarnold <78613198+markgarnold@users.noreply.github.com>
2024-11-09 12:23:01 -0500

    conf. in arnud.py

--- examples/arnud.py
+++ examples/arnud.py
@@ -12,10 +12,10 @@ import math
 import os
 import xlns as xl
 
-#import lpvip_ufunc       #uncomment for lpvip
-import xlnsudFracnorm  #uncomment for Zhang/Han or Paliouras fractional normalize
-xlnsudFracnorm.ilog2 = xlnsudFracnorm.ipallog2 #also uncomment only for Paliouras
-xlnsudFracnorm.ipow2 = xlnsudFracnorm.ipalpow2
+#import conf.lpvip_ufunc       #uncomment for lpvip
+import conf.xlnsudFracnorm  #uncomment for Zhang/Han or Paliouras fractional normalize
+conf.xlnsudFracnorm.ilog2 = conf.xlnsudFracnorm.ipallog2 #also uncomment only for Paliouras
+conf.xlnsudFracnorm.ipow2 = conf.xlnsudFracnorm.ipalpow2
 
 xl.xlnssetF(7)
 
