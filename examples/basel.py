4892c194b0c71c841824353de7fb18a07d564977
markgarnold <78613198+markgarnold@users.noreply.github.com>
2024-11-21 22:25:32 -0500

    Update basel.py

--- examples/basel.py
+++ examples/basel.py
@@ -5,24 +5,31 @@ import timeit
 
 def basel_sum(valuelist: list):
     valuelist = 1/(valuelist * valuelist)
-    return np.sum(valuelist)
+    return valuelist.sum()  #np.sum does not work with xlnsnp
 
 def run(terms: float):
     i = np.arange(1,terms)
     li = np.array(xl.xlnscopy(i))
+    xi = xl.xlnsnp(i)
     print(terms, "\t", end='')
 
     # floats
     start = timeit.default_timer()
-    fpep = math.sqrt(6 * basel_sum(i))
+    fpep = (6 * basel_sum(i))**.5
     print(f" FP err: {math.pi - fpep}\t", end='')
-    print(" time: ", timeit.default_timer() - start, end='')
+    #print(" time: ", timeit.default_timer() - start, end='')
 
     # xlns
     start = timeit.default_timer()
-    xlep = math.sqrt(6 * basel_sum(li))
-    print(f" LNS err: {math.pi - xlep}\t", end='')
-    print(" time: ", timeit.default_timer() - start)
+    xlep = (6 * basel_sum(li))**.5
+    print(f"xlns err: {math.pi - xlep}\t", end='')
+    #print(" time: ", timeit.default_timer() - start)
+
+    # xlnsnp
+    start = timeit.default_timer()
+    xlep = (6 * basel_sum(xi))**.5
+    print(f"xlnsnp e: {math.pi - xlep}\t")   #, end='')
+    #print(" time: ", timeit.default_timer() - start)
 
 def main():
     for k in range(2,6):
