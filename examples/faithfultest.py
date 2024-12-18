c79f0be5745de02b42d3ffe4d44e7147fed9cdcb
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2024-12-18 10:35:10 -0900

    Update faithfultest.py
    Indentation fix

--- examples/faithfultest.py
+++ examples/faithfultest.py
@@ -21,26 +21,26 @@ def faithful_sb_ufunc(f, ufunc):
             ((testufuncdb==truncdb).sum() + (testufuncdb==truncdb+1).sum())/len(x))
 
 def test_faithful_sb_interp():
-for f in [7,9,11,13]:
-#for f in [15]:
-#for f in [17]:
-  print("f="+str(f))
-  res = faithful_sb_ufunc(f,xl.sbdb_ufunc_ideal)
-  print(str(res)+" ideal sb db")
-  res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb)
-  print(str(res)+" interp sb db")
-  res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g2)
-  print(str(res)+" interp_g2 sb db")
-  res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g4)
-  print(str(res)+" interp_g4 sb db")
-  res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g6)
-  print(str(res)+" interp_g6 sb db")
-  res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g2_faith)
-  print(str(res)+" interp_g2 sb db faith")
-  res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g4_faith)
-  print(str(res)+" interp_g4 sb db faith")
-  res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g6_faith)
-  print(str(res)+" interp_g6 sb db faith")
+  for f in [7,9,11,13]:
+  #for f in [15]:
+  #for f in [17]:
+    print("f="+str(f))
+    res = faithful_sb_ufunc(f,xl.sbdb_ufunc_ideal)
+    print(str(res)+" ideal sb db")
+    res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb)
+    print(str(res)+" interp sb db")
+    res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g2)
+    print(str(res)+" interp_g2 sb db")
+    res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g4)
+    print(str(res)+" interp_g4 sb db")
+    res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g6)
+    print(str(res)+" interp_g6 sb db")
+    res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g2_faith)
+    print(str(res)+" interp_g2 sb db faith")
+    res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g4_faith)
+    print(str(res)+" interp_g4 sb db faith")
+    res = faithful_sb_ufunc(f,interp_cotran_ufunc.sbdb_ufunc_interpsbcotrdb_g6_faith)
+    print(str(res)+" interp_g6 sb db faith")
 
 test_faithful_sb_interp()
 
