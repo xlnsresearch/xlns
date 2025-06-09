48e924fc96ae976deec6dd07bca481ce52060f0e
akrentz6 <alexkrentz2@gmail.com>
2025-06-09 11:44:36 +0100

    more comparison operators

--- src/xlnstorch/operators/comparison_ops.py
+++ src/xlnstorch/operators/comparison_ops.py
@@ -43,7 +43,7 @@ def _lns_ge(x, y):
     x_packed_sign, y_packed_sign = x_packed & 1, y_packed & 1
 
     both_pos = (x_packed_sign == 0) & (y_packed_sign == 0)
-    result_both_pos = torch.ge(y_packed_log, y_packed_log)
+    result_both_pos = torch.ge(x_packed_log, y_packed_log)
 
     x_pos_y_neg = (x_packed_sign == 0) & (y_packed_sign == 1)
     result_x_pos = torch.ones_like(x_packed_sign, dtype=torch.bool)
@@ -51,7 +51,7 @@ def _lns_ge(x, y):
     x_neg_y_pos = (x_packed_sign == 1) & (y_packed_sign == 0)
     result_x_neg = torch.zeros_like(x_packed_sign, dtype=torch.bool)
 
-    # no need to check for both negative case, as it's the final else case
+    # no need to check explicitly for both negative case, as it's the final case
     result_both_neg = torch.ge(y_packed_log, x_packed_log)
 
     return torch.where(both_pos, result_both_pos,
@@ -63,6 +63,102 @@ def ge(x, y, *, out=None):
     x, y = format_lnstensor_operands(x, y)
     y._lns = y._lns.broadcast_to(x.shape)
     result = _lns_ge(x._lns, y._lns)
+
+    if out is not None:
+        out.copy_(result)
+
+    return result
+
+def _lns_gt(x, y):
+    x_packed, y_packed = x._lns, y._lns
+    x_packed_log, y_packed_log = x_packed >> 1, y_packed >> 1
+    x_packed_sign, y_packed_sign = x_packed & 1, y_packed & 1
+
+    both_pos = (x_packed_sign == 0) & (y_packed_sign == 0)
+    result_both_pos = torch.gt(x_packed_log, y_packed_log)
+
+    x_pos_y_neg = (x_packed_sign == 0) & (y_packed_sign == 1)
+    result_x_pos = torch.ones_like(x_packed_sign, dtype=torch.bool)
+
+    x_neg_y_pos = (x_packed_sign == 1) & (y_packed_sign == 0)
+    result_x_neg = torch.zeros_like(x_packed_sign, dtype=torch.bool)
+
+    # no need to check explicitly for both negative case, as it's the final case
+    result_both_neg = torch.gt(y_packed_log, x_packed_log)
+
+    return torch.where(both_pos, result_both_pos,
+           torch.where(x_pos_y_neg, result_x_pos,
+           torch.where(x_neg_y_pos, result_x_neg, result_both_neg)))
+
+@implements(torch.gt, _lns_gt, "default", default=True)
+def gt(x, y, *, out=None):
+    x, y = format_lnstensor_operands(x, y)
+    y._lns = y._lns.broadcast_to(x.shape)
+    result = _lns_gt(x._lns, y._lns)
+
+    if out is not None:
+        out.copy_(result)
+
+    return result
+
+def _lns_le(x, y):
+    x_packed, y_packed = x._lns, y._lns
+    x_packed_log, y_packed_log = x_packed >> 1, y_packed >> 1
+    x_packed_sign, y_packed_sign = x_packed & 1, y_packed & 1
+
+    both_pos = (x_packed_sign == 0) & (y_packed_sign == 0)
+    result_both_pos = torch.le(x_packed_log, y_packed_log)
+
+    x_pos_y_neg = (x_packed_sign == 0) & (y_packed_sign == 1)
+    result_x_pos = torch.ones_like(x_packed_sign, dtype=torch.bool)
+
+    x_neg_y_pos = (x_packed_sign == 1) & (y_packed_sign == 0)
+    result_x_neg = torch.zeros_like(x_packed_sign, dtype=torch.bool)
+
+    # no need to check explicitly for both negative case, as it's the final case
+    result_both_neg = torch.le(y_packed_log, x_packed_log)
+
+    return torch.where(both_pos, result_both_pos,
+           torch.where(x_pos_y_neg, result_x_pos,
+           torch.where(x_neg_y_pos, result_x_neg, result_both_neg)))
+
+@implements(torch.le, _lns_le, "default", default=True)
+def le(x, y, *, out=None):
+    x, y = format_lnstensor_operands(x, y)
+    y._lns = y._lns.broadcast_to(x.shape)
+    result = _lns_le(x._lns, y._lns)
+
+    if out is not None:
+        out.copy_(result)
+
+    return result
+
+def _lns_lt(x, y):
+    x_packed, y_packed = x._lns, y._lns
+    x_packed_log, y_packed_log = x_packed >> 1, y_packed >> 1
+    x_packed_sign, y_packed_sign = x_packed & 1, y_packed & 1
+
+    both_pos = (x_packed_sign == 0) & (y_packed_sign == 0)
+    result_both_pos = torch.lt(x_packed_log, y_packed_log)
+
+    x_pos_y_neg = (x_packed_sign == 0) & (y_packed_sign == 1)
+    result_x_pos = torch.ones_like(x_packed_sign, dtype=torch.bool)
+
+    x_neg_y_pos = (x_packed_sign == 1) & (y_packed_sign == 0)
+    result_x_neg = torch.zeros_like(x_packed_sign, dtype=torch.bool)
+
+    # no need to check explicitly for both negative case, as it's the final case
+    result_both_neg = torch.lt(y_packed_log, x_packed_log)
+
+    return torch.where(both_pos, result_both_pos,
+           torch.where(x_pos_y_neg, result_x_pos,
+           torch.where(x_neg_y_pos, result_x_neg, result_both_neg)))
+
+@implements(torch.lt, _lns_lt, "default", default=True)
+def lt(x, y, *, out=None):
+    x, y = format_lnstensor_operands(x, y)
+    y._lns = y._lns.broadcast_to(x.shape)
+    result = _lns_lt(x._lns, y._lns)
 
     if out is not None:
         out.copy_(result)
