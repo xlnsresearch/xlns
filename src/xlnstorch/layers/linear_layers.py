eae81163dd10c31c281747190f5c4fee7441a6c7
akrentz6 <alexkrentz2@gmail.com>
2025-06-19 15:51:33 +0100

    bilinear and lazylinear layers, and layer docstrings

--- src/xlnstorch/layers/linear_layers.py
+++ src/xlnstorch/layers/linear_layers.py
@@ -3,6 +3,11 @@ from . import LNSModule
 from .. import rand
 
 class LNSIdentity(LNSModule):
+    """
+    An LNS identity layer that does not change the input.
+
+    See also: `torch.nn.Identity`
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -11,6 +16,34 @@ class LNSIdentity(LNSModule):
         return input
 
 class LNSLinear(LNSModule):
+    """
+    An LNS linear layer that performs a linear transformation on the input, `x`.
+    It applies the affine linear transformation `y = xA^T + b`, where `A` is the
+    weight matrix, and `b` is the bias vector if bias is enabled.
+
+    See also: `torch.nn.Linear`
+
+    Parameters
+    ----------
+    in_features : int
+        The number of input features.
+    out_features : int
+        The number of output features.
+    bias : bool, optional
+        Whether to include a bias term in the transformation. Default is True.
+    device : torch.device, optional
+        The device on which to create the layer's parameters. If None, defaults to the current.
+
+    Variables
+    ---------
+    weight : LNSTensor
+        The weight matrix of shape `(out_features, in_features)`, initialized with random values
+        uniformly distributed between `-sqrt(k)` and `sqrt(k)`, where `k = 1 / in_features`.
+    bias : LNSTensor
+        The bias vector of shape `(out_features,)`, initialized with random values uniformly
+        distributed between `-sqrt(k)` and `sqrt(k)`, where `k = 1 / in_features`. This is only
+        created if `bias` is set to True.
+    """
 
     def __init__(self, in_features, out_features, bias=True, device=None):
         super().__init__()
@@ -27,4 +60,105 @@ class LNSLinear(LNSModule):
             self.bias = None
 
     def forward(self, x):
+        return torch.nn.functional.linear(x, self.weight, self.bias)
+
+class LNSBilinear(LNSModule):
+    """
+    An LNS bilinear layer that performs a bilinear transformation on two inputs, `x1` and `x2`.
+    It applies the transformation `y = x1^T A x2 + b`, where `A` is the weight tensor,
+    and `b` is the bias vector if bias is enabled.
+
+    See also: `torch.nn.Bilinear`
+
+    Parameters
+    ----------
+    in1_features : int
+        The number of input features for the first input.
+    in2_features : int
+        The number of input features for the second input.
+    out_features : int
+        The number of output features.
+    bias : bool, optional
+        Whether to include a bias term in the transformation. Default is True.
+    device : torch.device, optional
+        The device on which to create the layer's parameters. If None, defaults to the current
+        device.
+
+    Variables
+    ---------
+    weight : LNSTensor
+        The weight tensor of shape `(out_features, in1_features, in2_features)`, initialized
+        with random values uniformly distributed between `-sqrt(k)` and `sqrt(k)`, where
+        `k = 1 / in1_features`.
+    bias : LNSTensor
+        The bias vector of shape `(out_features,)`, initialized with random values uniformly
+        distributed between `-sqrt(k)` and `sqrt(k)`, where `k = 1 / (in1_features)`. This
+        is only created if `bias` is set to True.
+    """
+
+    def __init__(self, in1_features, in2_features, out_features, bias=True, device=None):
+        super().__init__()
+        self.in1_features = in1_features
+        self.in2_features = in2_features
+        self.out_features = out_features
+        self.has_bias = bias
+
+        sqrt_k = (1.0 / (in1_features + in2_features)) ** 0.5
+        self.register_parameter("weight", (rand(out_features, in1_features, in2_features, device=device) * 2 - 1) * sqrt_k)
+
+        if self.has_bias:
+            self.register_parameter("bias", (rand(out_features, device=device) * 2 - 1) * sqrt_k)
+        else:
+            self.bias = None
+
+    def forward(self, x1, x2):
+        return torch.nn.functional.bilinear(x1, x2, self.weight, self.bias)
+
+class LNSLazyLinear(LNSModule):
+    """
+    An LNS lazy linear layer that performs a linear transformation on the input, `x`,
+    without initializing the weight and bias parameters until the first forward pass.
+    The `in_features` argument is inferred from the `input.shape[-1]` during the first
+    call.
+
+    See also: `torch.nn.LazyLinear`
+
+    Parameters
+    ----------
+    out_features : int
+        The number of output features.
+    bias : bool, optional
+        Whether to include a bias term in the transformation. Default is True.
+    device : torch.device, optional
+        The device on which to create the layer's parameters. If None, defaults to the current
+        device.
+
+    Variables
+    ---------
+    weight : LNSTensor
+        The weight matrix of shape `(out_features, in_features)`, initialized with random values
+        uniformly distributed between `-sqrt(k)` and `sqrt(k)`, where `k = 1 / in_features`.
+    bias : LNSTensor
+        The bias vector of shape `(out_features,)`, initialized with random values uniformly
+        distributed between `-sqrt(k)` and `sqrt(k)`, where `k = 1 / in_features`. This is only
+        created if `bias` is set to True.
+    """
+
+    def __init__(self, out_features, bias=True, device=None):
+        super().__init__()
+        self.out_features = out_features
+        self.has_bias = bias
+        self.device = device
+        self.weight = None
+        self.bias = None
+
+    def forward(self, x):
+        if self.weight is None:
+            self.in_features = x.shape[-1]
+            sqrt_k = (1.0 / self.in_features) ** 0.5
+
+            self.register_parameter("weight", (rand(self.out_features, self.in_features, device=self.device) * 2 - 1) * sqrt_k)
+            if self.has_bias:
+                self.register_parameter("bias", (rand(self.out_features, device=self.device) * 2 - 1) * sqrt_k)
+
         return torch.nn.functional.linear(x, self.weight, self.bias)
