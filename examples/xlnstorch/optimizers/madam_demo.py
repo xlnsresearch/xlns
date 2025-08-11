7b6ecf87d84b7de8abadf46b3f07fb13b409049e
akrentz6 <alexkrentz2@gmail.com>
2025-08-11 16:48:36 +0100

    add lns madam demo example

--- examples/xlnstorch/optimizers/madam_demo.py
+++ examples/xlnstorch/optimizers/madam_demo.py
@@ -19,8 +19,8 @@ model_mul_pow.weight_lns.data.copy_(model_sgd.weight_lns.data)
 model_mul_pow.bias_lns.data.copy_(model_sgd.bias_lns.data)
 
 opt_sgd = xltorch.optim.LNSSGD(model_sgd.lns_parameters(), lr=0.1)
-opt_mul = xltorch.optim.LNSMul(model_mul.lns_parameters(), lr=0.05, use_pow=False)
-opt_mul_pow = xltorch.optim.LNSMul(model_mul_pow.lns_parameters(), lr=0.05, use_pow=True)
+opt_madam = xltorch.optim.LNSMadam(model_mul.lns_parameters(), lr=0.05, use_pow=False)
+opt_madam_pow = xltorch.optim.LNSMadam(model_mul_pow.lns_parameters(), lr=0.05, use_pow=True)
 loss_fn = torch.nn.MSELoss(reduction='mean')
 
 n_steps = 20
@@ -33,28 +33,28 @@ for step in range(1, n_steps + 1):
     loss_sgd.backward()
     opt_sgd.step()
 
-    opt_mul.zero_grad()
+    opt_madam.zero_grad()
     loss_mul = loss_fn(model_mul(inp), target)
     loss_mul.backward()
-    opt_mul.step()
+    opt_madam.step()
 
-    opt_mul_pow.zero_grad()
+    opt_madam_pow.zero_grad()
     loss_mul_pow = loss_fn(model_mul_pow(inp), target)
     loss_mul_pow.backward()
-    opt_mul_pow.step()
+    opt_madam_pow.step()
 
     loss_hist_sgd.append(loss_sgd.item())
     loss_hist_mul.append(loss_mul.item())
     loss_hist_mul_pow.append(loss_mul_pow.item())
 
     print(f"Iter {step:02d} | loss_SGD={loss_sgd.item():.2e} | "
-          f"loss_SignMul={loss_mul.item():.2e} | "
-          f"loss_SignMul_pow={loss_mul_pow.item():.2e}")
+          f"loss_Madam={loss_mul.item():.2e} | "
+          f"loss_Madam_pow={loss_mul_pow.item():.2e}")
 
 plt.figure(figsize=(6, 4))
 plt.plot(range(1, n_steps + 1), loss_hist_sgd, label='LNSSGD (lr=0.1)',  marker='o')
-plt.plot(range(1, n_steps + 1), loss_hist_mul, label='LNSMul (lr=0.05)', marker='s')
-plt.plot(range(1, n_steps + 1), loss_hist_mul_pow, label='LNSMul (lr=0.05, pow)', marker='^')
+plt.plot(range(1, n_steps + 1), loss_hist_mul, label='LNSMadam (lr=0.05)', marker='s')
+plt.plot(range(1, n_steps + 1), loss_hist_mul_pow, label='LNSMadam (lr=0.05, pow)', marker='^')
 plt.title("MSE loss vs. optimisation step")
 plt.xlabel("Step")
 plt.ylabel("MSE loss")
