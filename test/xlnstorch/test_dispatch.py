670e19394fed0397aaa6ef31970bc054589be0bc
akrentz6 <alexkrentz2@gmail.com>
2025-08-31 20:04:05 +0100

    update dispatch tests to support new LNSOps

--- test/xlnstorch/test_dispatch.py
+++ test/xlnstorch/test_dispatch.py
@@ -43,7 +43,8 @@ def test_apply_lns_op():
     lns2 = xltorch.lnstensor(data2, f=23)
 
     # Test with default implementation
-    result = xltorch.apply_lns_op(torch.add, lns1._lns.view(torch.int64), lns2._lns.view(torch.int64), lns1.base)
+    result = xltorch.apply_lns_op(torch.add, xltorch.ops.LNSOps(lns1.base),
+                                  lns1._lns.view(torch.int64), lns2._lns.view(torch.int64))
     result_lns = xltorch.lnstensor(result, from_lns=True, b=lns1.base)
     expected = lns1.value + lns2.value
     assert torch.allclose(result_lns.value, expected)
